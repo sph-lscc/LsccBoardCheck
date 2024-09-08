55406454603a94b02fdf25294748b6719505b486
Paul Hardy <paul.hardy@latticesemi.com>
2024-09-08 08:32:07 +0300

    Updated coding guidelines (Verible Linter)

--- rtl/led_kitt.sv
+++ rtl/led_kitt.sv
@@ -6,19 +6,18 @@
 `include "lscc_defines.svh"
 
 module led_kitt #(
-  parameter CLK_IN_MHZ   = 125,
-            LED_POLARITY = 1'b0  )
-(
-  input              clk_i,
-  input              rstn_i,
-  output logic [7:0] display_o
+    int CLK_IN_MHZ   = 125,
+    bit LED_POLARITY = 1'b0
+) (
+    input              clk_i,
+    input              rstn_i,
+    output logic [7:0] display_o
 );
 
 
-// Local Parameters
+  // Local Parameters
 
-  localparam [0:13] [7:0] LED_DISP = {
-    8'b00000001,
+  localparam bit [13:0][7:0] LEDDecoder = {
     8'b00000010,
     8'b00000100,
     8'b00001000,
@@ -31,47 +30,47 @@ module led_kitt #(
     8'b00010000,
     8'b00001000,
     8'b00000100,
-    8'b00000010
-  } ;
-  
-  localparam SYS_FREQ = CLK_IN_MHZ * 1000 * 1000 / 14;
+    8'b00000010,
+    8'b00000001
+  };
 
-// Signal Declarations
-  
-  logic [$clog2(SYS_FREQ)-1:0] prescaler;
-  logic                        prescaler_tc;
-  logic                  [3:0] display_counter;     
+  localparam int SysFreq = CLK_IN_MHZ * 1000 * 1000 / 14;
 
-// Module Behaviour
+  // Signal Declarations
+
+  logic [$clog2(SysFreq)-1:0] prescaler, prescaler_tc;
+  logic [3:0] display_counter;
+
+  // Module Behaviour
 
 `ifndef SIM
 
   // Prescaler generates 1Hz pulse to enable display counter
   always_ff @(posedge clk_i, negedge rstn_i) begin : prescale
-    if (!rstn_i) prescaler  <= 'b0;
-    else         prescaler  <= prescaler_tc ? 'b0 : prescaler + 1'b1;
+    if (!rstn_i) prescaler <= 'b0;
+    else prescaler <= prescaler_tc ? 'b0 : prescaler + 1'b1;
   end : prescale
-  
-  assign prescaler_tc = (prescaler == SYS_FREQ - 1) ;
 
-`else 
+  assign prescaler_tc = (prescaler == SysFreq - 1);
+
+`else
 
   // Disable prescaler for simulation
   assign prescaler    = 'b0;
-  assign prescaler_tc = 1'b1; 
+  assign prescaler_tc = 1'b1;
 
 `endif
 
   // Display Counter - Increment once per prescaler pulse to value 13
   always_ff @(posedge clk_i, negedge rstn_i) begin : dsply_cntr
     if (!rstn_i) display_counter <= 'b0;
-    else if (prescaler_tc) display_counter  <= display_counter > 12 ? 'b0 : display_counter + 1'b1;
+    else if (prescaler_tc) display_counter <= display_counter > 12 ? 'b0 : display_counter + 1'b1;
   end : dsply_cntr
 
   // Display Decoder
   always_ff @(posedge clk_i, negedge rstn_i) begin : dsply_dcdr
     if (!rstn_i) display_o <= 'b0;
-    else         display_o <= LED_POLARITY ? LED_DISP[display_counter] : ~LED_DISP[display_counter] ;
+    else display_o <= LED_POLARITY ? LEDDecoder[display_counter] : ~LEDDecoder[display_counter];
   end : dsply_dcdr
 
 endmodule
--- rtl/svn_seg_cntr.sv
+++ rtl/svn_seg_cntr.sv
@@ -1,78 +1,75 @@
 `include "lscc_defines.svh"
 
 module svn_seg_cntr #(
-  parameter CLK_IN_MHZ   = 125,
-            LED_POLARITY = 1'b0  )
-(
-  input              clk_i,
-  input              rstn_i,
-  output logic [7:0] display_o,
-  output logic [2:0] seg_sel_o 
+    byte CLK_IN_MHZ   = 125,
+    bit  LED_POLARITY = 1'b0
+) (
+    input              clk_i,
+    input              rstn_i,
+    output logic [7:0] seg_display_o,
+    output logic [2:0] seg_sel_o
 );
 
-// Local parameters & Constants
+  // Local parameters & Constants
 
   // 7 Segment display decoder
-  localparam [0:15] [7:0] SEG7DISP = {
-    8'b10111111, 
-    8'b00000110,
-    8'b01011011,
-    8'b01001111, 
-    8'b01100110,
-    8'b01101101,
-    8'b01111101,
-    8'b00000111,
-    8'b01111111,
-    8'b01101111,
-    8'b01110111,
-    8'b01111100,
-    8'b00111001,
-    8'b01011110,
+  localparam bit [15:0][7:0] SEG7DISP = {
+    8'b01110001,
     8'b01111001,
-    8'b01110001
-  } ;
-  
-  localparam SYS_FREQ = CLK_IN_MHZ * 1000 * 1000;
+    8'b01011110,
+    8'b00111001,
+    8'b01111100,
+    8'b01110111,
+    8'b01101111,
+    8'b01111111,
+    8'b00000111,
+    8'b01111101,
+    8'b01101101,
+    8'b01100110,
+    8'b01001111,
+    8'b01011011,
+    8'b00000110,
+    8'b10111111
+  };
 
-// Signal Declarations
-  
-  logic [$clog2(SYS_FREQ)-1:0] prescaler;
-  logic                        prescaler_tc;
-  logic                  [3:0] display_counter;
+  localparam int SysFreq = CLK_IN_MHZ * 1000 * 1000;
 
-// Module Behaviour
+  // Signal Declarations
+
+  logic [$clog2(SysFreq)-1:0] prescaler, prescaler_tc;
+  logic [                3:0] display_counter;
+
+  // Module Behaviour
 
 `ifndef SIM
 
   // Prescaler generates 1Hz pulse to enable display counter
   always_ff @(posedge clk_i, negedge rstn_i) begin : prescale
     if (!rstn_i) prescaler <= 'b0;
-    else         prescaler <= prescaler_tc ? 'b0 : prescaler + 1;
+    else prescaler <= prescaler_tc ? 'b0 : prescaler + 1;
   end : prescale
 
-  assign prescaler_tc = (prescaler == SYS_FREQ - 1) ;
-  
-`else 
+  assign prescaler_tc = (prescaler == SysFreq - 1);
+
+`else
 
   // Disable prescaler for simulation
   assign prescaler = 'b0;
-  assign prescaler_tc = 1'b1; 
+  assign prescaler_tc = 1'b1;
 
 `endif
 
   // Display Counter - Increment once per prescaler pulse
-  always_ff @(posedge clk_i, negedge rstn_i) begin : dsply_cntr
-    if (!rstn_i) display_counter <= 'b0;
-    else if (prescaler_tc) display_counter  <= display_counter + 1;
+  always_ff @(posedge clk_i) begin : dsply_cntr
+    if (prescaler_tc) display_counter <= display_counter + 1;
   end : dsply_cntr
 
   // Display Decoder - binary to 7 seg display
-  always_ff @(posedge clk_i, negedge rstn_i) begin : dsply_dcdr
-    if (!rstn_i) display_o <= 'b0;
-    else         display_o <= LED_POLARITY ? SEG7DISP[display_counter] : ~SEG7DISP[display_counter] ;
+  always_ff @(posedge clk_i) begin : dsply_dcdr
+    seg_display_o <= LED_POLARITY ? SEG7DISP[display_counter] : ~SEG7DISP[display_counter];
   end : dsply_dcdr
 
-  // Enable single digit of 3 x 7seg display
-  assign seg_sel_o = 3'b010;
+  // Enable ALL digits of 3 x 7seg display
+  assign seg_sel_o = 3'b111;
 
 endmodule
